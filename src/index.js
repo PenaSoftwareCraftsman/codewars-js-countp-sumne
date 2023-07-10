00a4b3aa9bba051b04ecd5ee7aaed3fa3b1881fe
PenaSoftwareCraftsman <jose.henrique.pena.goulart@gmail.com>
2023-07-10 19:07:15 -0300

    Feat: Counting numbers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-const countPositivesSumNegatives = () => {
-    const result = [];
+const countPositivesSumNegatives = (input) => {
+    let result = [];
 
+    let countPositives = 0; 
+
+    if(input === [] || input === null)
+        return result;
+    else
+    {  
+        for(let number in input){
+            countPositives++;
+        }
+    }
+
+    result.push(countPositives);
     return result;
 }
 
