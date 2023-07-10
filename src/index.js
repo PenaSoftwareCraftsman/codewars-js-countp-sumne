1c504f53c95c84eaf9a754120e4d3d4fff050c2b
PenaSoftwareCraftsman <jose.henrique.pena.goulart@gmail.com>
2023-07-10 18:33:08 -0300

    feat: Returning a empty array for a null input

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const countPositivesSumNegatives = () => {
+    const result = [];
+
+    return result;
+}
+
+module.exports = {
+    countPositivesSumNegatives
+}
--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,7 @@
+const {countPositivesSumNegatives} = require('./../src/index');
+
 describe("countPositivesSumNegatives", ()=>{
     it('Should return a empty array if the input is null', () => {
-        expect(countPositivesSumNegatives(null)).toBe([]);
+        expect(countPositivesSumNegatives(null)).toStrictEqual([]);
     });
 })
