db75cfd710551c9392ba1259e770e5c0e05d8aa2
PenaSoftwareCraftsman <jose.henrique.pena.goulart@gmail.com>
2023-07-10 18:42:30 -0300

    Test: Returning an empty array if the input is empty

--- test/index.test.js
+++ test/index.test.js
@@ -4,4 +4,8 @@ describe("countPositivesSumNegatives", ()=>{
     it('Should return a empty array if the input is null', () => {
         expect(countPositivesSumNegatives(null)).toStrictEqual([]);
     });
+
+    it('Should return an empty array if the input is empty', () => {
+        expect(countPositivesSumNegatives(null)).toStrictEqual([]);
+    });
 })
